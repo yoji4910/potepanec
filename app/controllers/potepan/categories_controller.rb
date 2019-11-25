5cd73cd7056b98f9a625da35afa2f7a7f171b127
YOJI4910 <fmn.outlook@outlook.com>
2019-11-25 15:49:36 +0900

    delete unnecessary comments in categories controller

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,8 +1,6 @@
 class Potepan::CategoriesController < ApplicationController
   def show
-    # 商品カテゴリー用ルートカテゴリー取得
     @categories = Spree::Taxonomy.includes(:root)
-    # 表示する商品を取得
     @taxon = Spree::Taxon.find(params[:id])
     @products = @taxon.products.includes(master: [:images, :default_price])
   end
