24d3c68d634d5b83cda41a50040393a525ec5053
YOJI4910 <fmn.outlook@outlook.com>
2019-11-26 17:24:14 +0900

    Change after PR2

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,6 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @categories = Spree::Taxonomy.includes(:root)
     @taxon = Spree::Taxon.find(params[:id])
-    @products = @taxon.products.includes(master: [:images, :default_price])
+    @products = @taxon.all_products.includes(master: [:images, :default_price])
   end
 end
