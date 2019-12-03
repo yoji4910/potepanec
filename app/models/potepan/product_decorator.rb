7aff5760aaf05f14861a4a764cde91222563db83
YOJI4910 <fmn.outlook@outlook.com>
2019-12-04 02:15:29 +0900

    Add constants.rb, move limit(..) from product_decorator to controller and improve space, indent

--- app/models/potepan/product_decorator.rb
+++ app/models/potepan/product_decorator.rb
@@ -1,11 +1,9 @@
 module Potepan::ProductDecorator
-  RELATED_PRODUCTS_LIMIT_NUMBER = 4
   def related_products
     Spree::Product.
       in_taxons(taxons).
       where.not(id: id).
-      distinct.includes(master: [:default_price, :images]).
-      limit(RELATED_PRODUCTS_LIMIT_NUMBER)
+      distinct.includes(master: [:default_price, :images])
   end
   Spree::Product.prepend self
 end
---
+++ config/initializers/constants.rb
@@ -0,0 +1,3 @@
+module Constants
+  RELATED_PRODUCTS_LIMIT_NUMBER = 4
+end
