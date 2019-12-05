f6a7cca2ba90fc2418eeaf85013a7029599275e5
YOJI4910 <fmn.outlook@outlook.com>
2019-12-05 21:32:33 +0900

    Add related_product_number test, move related_product_number constant to controller

--- config/initializers/constants.rb
+++
@@ -1,3 +0,0 @@
-module Constants
-  RELATED_PRODUCTS_LIMIT_NUMBER = 4
-end
--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -1,76 +1,134 @@
 require 'rails_helper'
 
 RSpec.describe '商品ページ', type: :system do
-  (1..3).each do |i|
-    price = i * 100
-    let("product#{i}".to_sym) do
-      create(
-        :product,
-        name: "Test_product_#{i}",
-        price: price,
-        description: "nice_item_#{i}"
-      )
-    end
-  end
   let(:taxonomy) { create(:taxonomy, name: 'taxonomy') }
-  let(:taxon1) { create(:taxon, name: 'taxon1', taxonomy: taxonomy, parent_id: taxonomy.root.id) }
-  let(:taxon2) { create(:taxon, name: 'taxon2', taxonomy: taxonomy, parent_id: taxonomy.root.id) }
+  let(:taxon1) { create(:taxon, name: 'bag', taxonomy: taxonomy, parent_id: taxonomy.root.id) }
+  let(:taxon2) { create(:taxon, name: 'shirt', taxonomy: taxonomy, parent_id: taxonomy.root.id) }
+  let(:product_of_taxon1) do
+    create(:product,
+           name: 'test_bag',
+           price: 100,
+           description: "nice_bag",
+           taxons: [taxon1])
+  end
+  let(:product_of_taxon2) do
+    create(:product,
+           name: 'test_shirt',
+           price: 200,
+           description: "nice_shirt",
+           taxons: [taxon2])
+  end
+  let(:related_products_of_taxon1) { create_list(:product, 3, taxons: [taxon1]) }
+  let(:related_products_of_taxon2) { create_list(:product, 5, taxons: [taxon2]) }
   let(:variant) { create(:variant, is_master: false, price: 100) }
 
   before do
-    product1.taxons << taxon1
-    product2.taxons << taxon2
-    product3.taxons << taxon1
-    product1.variants << variant
+    related_products_of_taxon1.each.with_index(1) do |product, i|
+      product.name = "#{taxon1.name}-#{i}"
+      product.price = product_of_taxon1.price + i
+      product.reload
+    end
+
+    related_products_of_taxon2.each.with_index(1) do |product, i|
+      product.name = "#{taxon2.name}-#{i}"
+      product.price = product_of_taxon2.price + i
+      product.reload
+    end
+
+    product_of_taxon1.variants << variant
   end
 
-  describe 'GET #show' do
+  describe '商品情報表示機能' do
+    before do
+      visit potepan_product_path(product_of_taxon1)
+    end
+
+    it "商品名が表示される" do
+      expect(page).to have_content product_of_taxon1.name
+    end
+
+    it "商品価格が表示される" do
+      expect(page).to have_content variant.display_price.to_s
+    end
+
+    it "商品説明が表示される" do
+      expect(page).to have_content product_of_taxon1.description
+    end
+
+    it "「一覧ページへ戻る」に所属カテゴリーへのリンクがセットされている" do
+      click_on '一覧ページへ戻る'
+      expect(current_url).to eq potepan_category_url(product_of_taxon1.taxon_ids[0])
+    end
+  end
+
+  describe 'バリエーション選択ボックス表示機能' do
     context "productがvariantをもつ時" do
-      before do
-        visit potepan_product_path(product1)
-      end
-
-      it "商品名が表示される" do
-        expect(page).to have_content product1.name
-      end
-
-      it "商品価格が表示される" do
-        expect(page).to have_content variant.display_price.to_s
-      end
-
-      it "商品説明が表示される" do
-        expect(page).to have_content product1.description
-      end
-
       it "optionのセレクトボックスが表示される" do
+        visit potepan_product_path(product_of_taxon1)
         expect(page).to have_css('#guiest_id3')
-      end
-
-      it "product1の関連(同一toxon_id)商品が表示される" do
-        expect(page).to have_content product3.name
-        expect(page).to have_content product3.display_price.to_s
-      end
-
-      it "product1の非関連(異なるtoxon_id)商品が表示されない" do
-        expect(page).to have_no_content product2.name
-        expect(page).to have_no_content product2.display_price.to_s
-      end
-
-      it "product1自身が関連商品に表示されない" do
-        expect(find('.productsContent h5')).to have_no_content product1.name
-        expect(find('.productsContent h3')).to have_no_content product1.display_price.to_s
-      end
-
-      it "関連商品に期待されるリンクがセットされている" do
-        click_on product3.name
-        expect(current_url).to eq potepan_product_url(product3)
       end
     end
 
     context "productがvariantをもたない時" do
       it "optionのセレクトボックスが表示されない" do
-        visit potepan_product_path(product2)
+        visit potepan_product_path(product_of_taxon2)
         expect(page).to have_no_css('#guiest_id3')
+      end
+    end
+  end
+
+  describe '関連商品表示機能' do
+    context '関連商品が3つ(4つ未満)の時' do
+      before do
+        visit potepan_product_path(product_of_taxon1)
+      end
+
+      it "product_of_taxon1の関連商品が3つ表示される" do
+        expect(page.all('.productBox').size).to eq 3
+      end
+
+      it "product_of_taxon1の非関連(taxon2の)商品が表示されない" do
+        related_products_of_taxon2.each do |product|
+          expect(page).to have_no_content product.name
+        end
+      end
+
+      it "product_of_taxon1自身が関連商品に表示されない" do
+        expect(all('.productsContent h5')).to have_no_content product_of_taxon1.name
+        expect(all('.productsContent h3')).to have_no_content product_of_taxon1.display_price.to_s
+      end
+
+      it "関連商品に期待されるリンクがセットされている" do
+        one_product = related_products_of_taxon1[0]
+        click_on one_product.name
+        expect(current_url).to eq potepan_product_url(one_product)
+      end
+    end
+
+    context '関連商品が5つ(4つ以上)の時' do
+      before do
+        visit potepan_product_path(product_of_taxon2)
+      end
+
+      it "product_of_taxon2の関連商品が4つ表示される" do
+        expect(page.all('.productBox').size).to eq 4
+      end
+
+      it "product_of_taxon2の非関連(taxon1の)商品が表示されない" do
+        related_products_of_taxon1.each do |product|
+          expect(page).to have_no_content product.name
+        end
+      end
+
+      it "product_of_taxon2自身が関連商品に表示されない" do
+        expect(all('.productsContent h5')).to have_no_content product_of_taxon2.name
+        expect(all('.productsContent h3')).to have_no_content product_of_taxon2.display_price.to_s
+      end
+
+      it "関連商品に期待されるリンクがセットされている" do
+        one_product = related_products_of_taxon2[0]
+        click_on one_product.name
+        expect(current_url).to eq potepan_product_url(one_product)
       end
     end
   end
